9c6df7a9ffad0e0f70ffaf3ebf29ab0226eff923
EdwEd <EdwEd>
2020-10-30 10:42:50 +0300

    Add project files.

---
+++ Data/Pixel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyPhotoshop.Data
+{
+    public class Pixel
+    {
+        public Pixel(double red, double green, double blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+
+        //Red,Green and blue means saturation of each channel from 0 to 1 (converted from 0 to 255 format)
+        //
+        double red;
+        double green;
+        double blue;
+        public double Red 
+        { 
+            get { return red; } 
+            set 
+            {
+                red= CorrectingValue(value);
+            } 
+        }
+        public double Green
+        {
+            get { return green; }
+            set
+            {
+               green= CorrectingValue(value);
+            }
+        }
+        public double Blue
+        {
+            get { return blue; }
+            set
+            {
+               blue= CorrectingValue(value);
+            }
+        }
+    
+        public double CorrectingValue (double value)
+        {
+            if (value < 0)
+                return 0;
+            if (value > 1)
+                return 1;
+            return value;
+        }
+    }
+}
---
+++ Filters/LighteningFilter.cs
@@ -0,0 +1,41 @@
+using MyPhotoshop.Data;
+using System;
+
+namespace MyPhotoshop
+{
+	public class LighteningFilter : IFilter
+	{
+		public ParameterInfo[] GetParameters()
+		{
+			return new []
+			{
+				new ParameterInfo { Name="Коэффициент", MaxValue=10, MinValue=0, Increment=0.1, DefaultValue=1 }
+				
+			};
+		}
+		
+		public override string ToString ()
+		{
+			return "Осветление/затемнение";
+		}
+		
+		public Photo Process(Photo original, double[] parameters)
+		{
+			var result=new Photo();
+			result.width=original.width;
+			result.height=original.height;
+			result.data=new Pixel[result.width,result.height];
+			
+			for (int x=0;x<result.width;x++)
+				for (int y=0;y<result.height;y++)
+                {
+					result.data[x, y] = new Pixel(original.data[x, y].Red * parameters[0],
+												original.data[x, y].Green * parameters[0],
+												original.data[x, y].Blue * parameters[0]);
+				}
+						
+			return result;
+		}
+	}
+}
+
